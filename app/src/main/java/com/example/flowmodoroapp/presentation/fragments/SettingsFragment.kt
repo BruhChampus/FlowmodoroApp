b9220d668a08b68ce9ea290b2087c55703074eb9
BruhChampus <snakzhave@gmail.com>
2023-07-02 14:36:02 +0300

    Do some changes Add animations for fragments Add dark mode switch

--- app/src/main/java/com/example/flowmodoroapp/presentation/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/flowmodoroapp/presentation/fragments/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.flowmodoroapp.presentation.fragments
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import com.example.flowmodoroapp.R
 import com.example.flowmodoroapp.databinding.FragmentSettingsBinding
@@ -24,6 +26,15 @@ class SettingsFragment : Fragment() {
         }
         binding.ivResults.setOnClickListener {
             it.findNavController().navigate(R.id.resultsScreenFragment)
+        }
+
+        binding.scSwitchNightMode.setOnCheckedChangeListener{buttonview, isChecked ->
+            val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            if (nightModeFlags == Configuration.UI_MODE_NIGHT_NO && isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
         }
 
         return binding.root
